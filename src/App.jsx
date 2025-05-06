d797d9edddc45c8a752d16171b1b867ea6363184
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-06 15:42:12 +0500

    Resolve Home page path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Edit from './pages/Edit';
 
 function App() 
